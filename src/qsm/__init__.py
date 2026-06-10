fe965bca0d5404c5d3d9d07b06bb0da3c0db73bb
aaron-jencks <aaronjencks@aol.com>
2026-06-10 13:35:58 -0400

    removed dead version code

--- src/qsm/__init__.py
+++ src/qsm/__init__.py
@@ -1,11 +1,9 @@
 """Queued state machine package."""
 
-from ._version import __version__
 from .qsm import QSM
 from .states import State, StateContext
 
 __all__ = [
-    "__version__",
     "QSM",
     "State",
     "StateContext",
