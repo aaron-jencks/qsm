328c39db850a2fd8d82e537f0bc74251368bfb02
aaron-jencks <aaronjencks@aol.com>
2026-06-10 12:25:57 -0400

    changed queue to accept multiple states at once

--- src/qsm/deque.py
+++ src/qsm/deque.py
@@ -4,22 +4,24 @@ from typing import Optional
 
 
 class StringDequeQueue(Queue[str]):
-    def put(self, item: str, block: bool = True, timeout: Optional[float] = None) -> None:
-        self.append(item, block=block, timeout=timeout)
+    def put(self, *items: str, block: bool = True, timeout: Optional[float] = None) -> None:
+        self.append(*items, block=block, timeout=timeout)
 
-    def append(self, item: str, block: bool = True, timeout: Optional[float] = None) -> None:
-        self._put_string(item, left=False, block=block, timeout=timeout)
+    def append(self, *items: str, block: bool = True, timeout: Optional[float] = None) -> None:
+        for item in items:
+            self._put_string(item, left=False, block=block, timeout=timeout)
 
-    def prepend(self, item: str, block: bool = True, timeout: Optional[float] = None) -> None:
-        self._put_string(item, left=True, block=block, timeout=timeout)
+    def prepend(self, *items: str, block: bool = True, timeout: Optional[float] = None) -> None:
+        for item in items:
+            self._put_string(item, left=True, block=block, timeout=timeout)
 
-    def append_nowait(self, item: str) -> None:
-        self.append(item, block=False)
+    def append_nowait(self, *items: str) -> None:
+        self.append(*items, block=False)
 
-    def prepend_nowait(self, item: str) -> None:
-        self.prepend(item, block=False)
+    def prepend_nowait(self, *items: str) -> None:
+        self.prepend(*items, block=False)
 
-    def _put_string(self, item: str, *, left: bool, block: bool, timeout: Optional[float]) -> None:
+    def _put_string(self, item: str, left: bool, block: bool, timeout: Optional[float]) -> None:
         if not isinstance(item, str):
             raise TypeError("StringDequeQueue only accepts strings")
         if timeout is not None and timeout < 0:
